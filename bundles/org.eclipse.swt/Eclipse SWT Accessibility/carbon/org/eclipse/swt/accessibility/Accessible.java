e00344ef94fb20d9e09fba1faad2d779352c9ce1
Carolyn MacLeod <carolyn>
2009-11-11 17:54:40 +0000

    change add/remove relation api to only have a single target

--- bundles/org.eclipse.swt/Eclipse SWT Accessibility/carbon/org/eclipse/swt/accessibility/Accessible.java
+++ bundles/org.eclipse.swt/Eclipse SWT Accessibility/carbon/org/eclipse/swt/accessibility/Accessible.java
@@ -427,15 +427,15 @@ public class Accessible {
 	}
 
 	/**
-	 * Adds a relation with the specified type and targets
+	 * Adds a relation with the specified type and target
 	 * to the receiver's set of relations.
 	 * 
 	 * @param type a constant beginning with RELATION_* indicating the type of relation
-	 * @param targets the accessible(s) that are targets for this relation
+	 * @param target the accessible that is the target for this relation
 	 * 
 	 * @since 3.6
 	 */
-	public void addAccessibleRelation(int type, Accessible targets[]) {
+	public void addAccessibleRelation(int type, Accessible target) {
 		//TODO: platform-specific? (we will manage the set on Windows)
 	}
 	
@@ -1587,15 +1587,15 @@ public class Accessible {
 	}
 
 	/**
-	 * Removes the relation with the specified type and targets
+	 * Removes the relation with the specified type and target
 	 * from the receiver's set of relations.
 	 * 
 	 * @param type a constant beginning with RELATION_* indicating the type of relation
-	 * @param targets the accessible(s) that are targets for this relation
+	 * @param target the accessible that is the target for this relation
 	 * 
 	 * @since 3.6
 	 */
-	public void removeAccessibleRelation(int type, Accessible targets[]) {
+	public void removeAccessibleRelation(int type, Accessible target) {
 		//TODO: platform-specific? (we will manage the set on Windows)
 	}
 	
