da8b8f771d5354460bd40c3c2b396a9329aed7f1
Carolyn MacLeod <carolyn>
2009-11-24 20:09:43 +0000

    initial

---
+++ examples/org.eclipse.swt.examples/src/org/eclipse/swt/examples/accessibility/AccessibleActionExample.java
@@ -0,0 +1,71 @@
+package org.eclipse.swt.examples.accessibility;
+
+import org.eclipse.swt.*;
+import org.eclipse.swt.widgets.*;
+import org.eclipse.swt.layout.*;
+import org.eclipse.swt.events.*;
+import org.eclipse.swt.graphics.*;
+import org.eclipse.swt.accessibility.*;
+
+public class AccessibleActionExample {
+	public static void main(String[] args) {
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setLayout(new GridLayout());
+		shell.setText("Accessible Action");
+		
+		final Canvas customButton = new Canvas(shell, SWT.NONE);
+		customButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		customButton.addPaintListener(new PaintListener() {
+			public void paintControl(PaintEvent e) {
+				String buttonText = "Hello Button";
+				Rectangle clientArea = customButton.getClientArea();
+				Point stringExtent = e.gc.stringExtent(buttonText);
+				int x = clientArea.x + (clientArea.width - stringExtent.x) / 2;
+				int y = clientArea.y + (clientArea.height - stringExtent.y) / 2;
+				e.gc.drawString(buttonText, x, y);
+			}
+		});
+		customButton.addMouseListener(new MouseAdapter() {
+			public void mouseDown(MouseEvent e) {
+				customButtonAction();
+			}
+		});
+
+		Accessible accessible = customButton.getAccessible();
+		accessible.addAccessibleListener(new AccessibleAdapter() {
+			public void getKeyboardShortcut(AccessibleEvent e) {
+				e.result = "ALT+P"; 
+			}
+		});
+		accessible.addAccessibleActionListener(new AccessibleActionAdapter() {
+			public void getActionCount(AccessibleActionEvent e) {
+				e.count = 1;
+			}
+			public void getName(AccessibleActionEvent e) {
+				e.string = "Press";
+			}
+			public void getDescription(AccessibleActionEvent e) {
+				e.string = "Press custom button";
+			}
+			public void doAction(AccessibleActionEvent e) {
+				System.out.println("Performing action " + e.index + " for " + customButton);
+				customButtonAction();
+			}
+			public void getKeyBinding(AccessibleActionEvent e) {
+				e.string = "P;CTRL+P";
+			}
+		});
+
+		shell.pack();
+		shell.open();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) display.sleep();
+		}
+		display.dispose();
+	}
+	
+	static void customButtonAction() {
+		System.out.println("custom button click");
+	}
+}
