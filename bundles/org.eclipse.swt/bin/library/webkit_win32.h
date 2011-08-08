53e0193e1e395a5b1acff8ea72f5f66aff763efc
Felipe Heidrich <Felipe_Heidrich@ca.ibm.com>
2011-08-08 13:39:00 -0400

    add missing files to branch

---
+++ bundles/org.eclipse.swt/bin/library/webkit_win32.h
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2010 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+#ifndef INC_webkit_win32_H
+#define INC_webkit_win32_H
+
+#include <ole2.h>
+#include <JavaScriptCore/JSContextRef.h>
+#include <JavaScriptCore/JSObjectRef.h>
+#include <JavaScriptCore/JSStringRef.h>
+#include <CFNetwork/CFHTTPCookiesPriv.h>
+#include <CFNetwork/CFURLRequestPriv.h>
+#include <CoreFoundation/CoreFoundation.h>
+#include <WebKitCOMAPI.h>
+#include "webkit_win32_custom.h"
+
+#endif /* INC_webkit_win32_H */
+
